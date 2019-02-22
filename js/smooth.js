343336fdff9e582d507f724e23883f01ca9660cd
kshitij kumar <kshitijk83@gmail.com>
2019-02-22 22:25:45 +0530

    adding slider

---
+++ js/smooth.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var sHome= document.getElementById('smooth-home');
+    var sTeam= document.getElementById('smooth-team');
+    var sCourses= document.getElementById('smooth-courses');
+
+    let j =0;
+    var text = document.querySelectorAll('.header_list');
+    sHome.addEventListener('click', function(){
+        smoothScroll('.header', 1000);
+    })
+    sTeam.addEventListener('click', function(){
+        smoothScroll('.team', 1000);
+    })
+    sCourses.addEventListener('click', function(){
+        smoothScroll('.courses', 1000);
+    })
+
+    $('.header_slider').slick({
+        slidesToShow: 1 ,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 800,
+        dots: true,
+    });
+
+    $('.header_slider').on('afterChange', function(event, slick, currentSlide){
+        text[j].classList.remove("active");
+        j++;
+        if(j==4){
+            j=0;
+        }
+        text[j].classList.add('active');
+    });
+    
+
+    var element = document.getElementsByClassName("slick-active");
+        var i;
+        for (i = 0; i < element.length; i++) {
+            element[1].classList.add("slick-center")
+        }
+
+})
+
+// function for smooth scrolling
+function smoothScroll(target, duration){
+    var target = document.querySelector(target);
+    var targetPos = target.getBoundingClientRect().top;
+    var startPos = window.pageYOffset;
+    var distance = targetPos-startPos;
+    var startTime =null;
+
+
+    function animation(currentTime){
+        if(!startTime) startTime = currentTime;
+        var timeElapsed = currentTime-startTime;
+        var run = ease(timeElapsed, startPos, distance, duration);
+        window.scrollTo(0, run);
+        if(timeElapsed<duration) {requestAnimationFrame(animation);}
+    }
+
+    function ease(t, b, c, d) {
+        return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+    };
+
+    requestAnimationFrame(animation);
+}
