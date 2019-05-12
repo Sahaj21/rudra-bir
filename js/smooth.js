af2f56c1d372afcb7cbb43c9986e244b87934c11
kshitij kumar <kshitijk83@gmail.com>
2019-05-12 21:40:20 +0530

    team and reach section responsive

--- js/smooth.js
+++ js/smooth.js
@@ -52,9 +52,29 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
     $('.courseSlider').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
+        centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
     });
 
     
