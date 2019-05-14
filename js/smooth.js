249bb8288e5eed484df457a04c29074979746510
kshitij kumar <kshitijk83@gmail.com>
2019-05-14 10:42:13 +0530

    slider required sections responsive

--- js/smooth.js
+++ js/smooth.js
@@ -51,25 +51,25 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 
-    $('.courseSlider').slick({
+    $('.courseSlide').slick({
         dots: true,
         infinite: false,
         speed: 300,
         slidesToShow: 3,
         slidesToScroll: 3,
         responsive: [
+        //     {
+        //     breakpoint: 900,
+        //     settings: {
+        //         slidesToShow: 1,
+        //         slidesToScroll: 1
+        //     }
+        //     },
             {
             breakpoint: 600,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3
-            }
-            },
-            {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
             }
         ]
